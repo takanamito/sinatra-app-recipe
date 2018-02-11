0dc81c835ebf3f9495f0f70c567b40fe06514bba
takanamito <takanamito0928@gmail.com>
2018-02-11 23:52:34 +0900

    Add nginx conf

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -1,7 +1,16 @@
 package 'nginx'
 
-# unicorn設定
+%w( /etc/nginx/nginx.conf /etc/nginx/conf.d/app.conf).each do |conf|
+  remote_file conf do
+    user 'root'
+    owner 'root'
+    mode '644'
+  end
+end
 
+directory '/var/log/nginx/app' do
+  mode '755'
+end
 
 service 'nginx' do
   action :restart
